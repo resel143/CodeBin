2e0699b9a4ca36a0488e3fcc7dc0c076caca6164
Reshul <53306036+resel143@users.noreply.github.com>
2021-02-28 19:34:06 +0530

    Python Game

---
+++ game.py
@@ -0,0 +1,11 @@
+import random
+print("Enter your name:")
+name=input()
+print("Hello "+name+"!!")
+print("Do you want to play Cricket with me?")
+enter=input()
+if enter[0]=="y" or "Y":
+	print("Let's Begin!!!!!! ")
+else:
+	print("Ok Thankyou")
+
